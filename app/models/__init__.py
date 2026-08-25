57d54b0c487c64c9d3e985f3e7bdd8fa77a1408d
hoanglong2305 <hivair21@gmail.com>
2026-08-25 08:42:44 +0700

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.activity import ActivityModel
-from app.models.club import ClubModel
-from app.models.user import UserModel
-from app.models.club_member import MemberModel
--- app/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user import UserBase, UserCreate, UserUpdate, UserResponse
-from .club import ClubBase, ClubUpdate, ClubResponse, ClubMemberCreate, ClubMemberResponse
-from .activity import ActivityBase, ActivityCreate, ActivityUpdate, ActivityResponse
